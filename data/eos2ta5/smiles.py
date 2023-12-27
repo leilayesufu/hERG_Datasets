afe299191208038796247d722511da1ac60f4d85
leilayesufu <leilayesufu2002@gmail.com>
2023-12-27 11:32:14 +0100

    commit

--- data/eos2ta5/smiles.py
+++ data/eos2ta5/smiles.py
@@ -5,14 +5,22 @@ from rdkit.Chem import inchi
 # Load the CSV file
 df = pd.read_csv('eos2ta5.csv')
 
+# Check if 'smiles' column exists (case-insensitive)
+smiles_column = 'smiles'
+if smiles_column.lower() not in map(str.lower, df.columns):
+    raise ValueError(f"The '{smiles_column}' column is not present in the CSV file.")
+
 # Define a function to convert SMILES to InChIKey
 def smiles_to_inchikey(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    inchi_key = inchi.MolToInchiKey(mol)
-    return inchi_key
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+        inchi_key = inchi.MolToInchiKey(mol)
+        return inchi_key
+    except Exception as e:
+        return f"Error: {e}"
 
 # Create a new column for InChIKey
-df['InChIKey'] = df['SMILES'].apply(smiles_to_inchikey)
+df['InChIKey'] = df['smiles'].apply(smiles_to_inchikey)
 
 # Save the updated DataFrame to a new CSV file
 df.to_csv('output_file.csv', index=False)
